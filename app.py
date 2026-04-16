51b8b1507dc166d3d6a774b9a4e5041b207761b5
win050801 <91003576+win050801@users.noreply.github.com>
2026-04-16 04:20:16 +0000

    v6

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def generate_content(content, model_name):
         ---
         {content}
         ---
-        Dựa trên kịch bản trên, hãy trình bày nội dung theo đúng cấu trúc sau thêm vào những từ ngữ seb tìm kiểm để tăng khả năng tiếp cận người xem:
+        Dựa trên kịch bản trên, hãy trình bày nội dung bằng tiếng Việt theo đúng cấu trúc sau thêm vào những từ ngữ SEO tìm kiểm để tăng khả năng tiếp cận người xem:
 
         1. **TIÊU ĐỀ VIDEO (3 Lựa chọn)**:
            - Đưa lên đầu tiên. Tiêu đề cần kịch tính, chuẩn SEO, chứa từ khóa mạnh dễ tìm kiếm.
